a696bc582ef9840c7485a53835eba3a2242dfd65
mingyu46 <edwardgao46@yahoo.com.au>
2016-06-07 13:34:07 +1000

    stream

---
+++ gender_project/predict and add gender/authentication.py
@@ -0,0 +1,14 @@
+class authentication:
+	def __init__(self):
+		self.consumer_key ="XV26sgQTgf9PmwQdXMfP8IJAe"
+		self.consumer_secret="Z0n3a8hYYDBzZGODmdFlbjBpuvT147ful9tOacmTRUNXD40Qrr"
+		self.access_token="2849298625-9yLNwHTloUnTtt8WZ2srob8PGNYdoethIx6lssc"
+		self.access_token_secret="ZFwW9SQhTzC1SNluEtSovM2FuE6N9iZwtSkwhgDQ9zviY"
+	def getconsumer_key(self):
+		return self.consumer_key
+	def getconsumer_secret(self):
+		return self.consumer_secret
+	def getaccess_token(self):
+		return self.access_token
+	def getaccess_token_secret(self):
+		return self.access_token_secret
